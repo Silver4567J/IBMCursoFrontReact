1b2dc4a40e60702596bab078d3282a857db45892
Silver4567J <silgold54@gmail.com>
2025-01-08 13:11:16 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+function App(props) {
+  const currDate = new Date();
+
+  return (
+    <div>
+      <h1>Hello, Javier Juventino!</h1>
+      <h2>Hoy es {currDate.toLocaleDateString()} y la hora es: {currDate.toLocaleTimeString()}.</h2>
+    </div>
+  );
+}
+
+export default App;

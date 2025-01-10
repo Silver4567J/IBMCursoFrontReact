9d2636c9bf96c6fb2caa28aed1f03af3583b2dfb
Silver4567J <silgold54@gmail.com>
2025-01-10 15:29:31 -0500

    Ejericio3

---
+++ Ejercicio3/src/Components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+const Footer = () => {
+    return (
+        <>
+            <footer>
+                <p>&copy; Organizacion de planeacion de eventos. Todos los derechos reservados.</p>
+            </footer>
+        </>
+    )
+}
+
+export default Footer

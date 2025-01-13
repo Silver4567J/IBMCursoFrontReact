cb6a07b5c9a387c4406c8f8b122129f100b99d9c
Silver4567J <silgold54@gmail.com>
2025-01-13 16:09:56 -0500

    Ejercicio5

--- Ejercicio5/src/Components/FetchYogaData.jsx
+++ Ejercicio5/src/Components/FetchYogaData.jsx
@@ -13,10 +13,10 @@ const FetchYogaData = () => {
                 {data && data.map((e) => (
                     <>
                         <li className='list_data'>
-                            <h3>{e.name}</h3>
-                            <p><strong>Importance: </strong>{e.importance}</p>
-                            <p><strong>Benefits: </strong>{e.benefits6}</p>
-                            <p><strong>Time to eat: </strong>{e.best_time_to_intake}</p>
+                            <h3><strong>Nombre: </strong>{e.name}</h3>
+                            <p><strong>Beneficios: </strong>{e.benefits}</p>
+                            <p><strong>Tiempo de duracion: </strong>{e.time_duration}</p>
+                            {/* <p><strong>Time to eat: </strong>{e.best_time_to_intake}</p> */}
                         </li>
                     </>
                 ))}

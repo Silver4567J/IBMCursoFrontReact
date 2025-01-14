0e55164dcfd23b50a91c233c18b42943954d2efb
Silver4567J <silgold54@gmail.com>
2025-01-14 16:50:06 -0500

    Ejercicio7

--- Ejercicio7/src/App.jsx
+++ Ejercicio7/src/App.jsx
@@ -3,16 +3,19 @@ import React from 'react';
 import ProductList from './Components/ProductList';
 import ShoppingCart from './Components/ShoppingCart';
 import './App.css'
-const App = () => {
-  return (
- 
-      <div>
-        <h1 className='app-heading'>E-Commerce Application</h1>
-        <ProductList />
-        <ShoppingCart />
-      </div>
+import SuperCoin from './Components/SuperCoin';
 
-  );
+const App = () => {
+    return (
+
+        <div>
+            <h1 className='app-heading'>E-Commerce Application</h1>
+            <ProductList />
+            <ShoppingCart />
+            <SuperCoin />
+        </div>
+
+    );
 };
 
 export default App;
